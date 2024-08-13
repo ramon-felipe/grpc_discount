d81524111523e6b799e5864f1ad9c28df6a44ea7
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2024-08-13 20:27:48 +0100

    adding DiscountCode tests

---
+++ DiscountGenerator.Tests.Unit/Domain/DiscountCodeTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using GrpcDiscountGenerator.Domain.ValueObjects;
+
+namespace DiscountGenerator.Tests.Unit.Domain;
+
+public sealed class DiscountCodeTests
+{
+	[Theory]
+	[InlineData("abcdefg")]
+	public void Should_CreateDiscountCode_Successfully(string code)
+	{
+		// Act
+		var discountCodeResult = DiscountCode.Create(code);
+
+		// Assert
+		discountCodeResult.Should().Succeed();
+		var discountCode = discountCodeResult.Value;
+
+		discountCode.Value.Should().Be(code);
+	}
+
+	[Fact]
+	public void Should_CreateEmptyDiscountCode_Successfully()
+	{
+        // Act
+        var discountCode = DiscountCode.CreateEmpty();
+
+		// Assert
+		discountCode.Should().NotBeNull();
+		discountCode.Value.Should().BeEmpty();
+    }
+}
+
